8326710b3af24a7a314420f38b5f44115f72d91a
João Otávio Biondo <joao.otb@gmail.com>
2021-11-21 20:11:52 -0300

    Use HTTPClient to send requests

--- lib/snap/exceptions/http_error.ex
+++ lib/snap/exceptions/http_error.ex
@@ -3,6 +3,8 @@ defmodule Snap.HTTPError do
   Represents an HTTP error returned while executing a query.
   """
 
+  alias Snap.HTTPClient
+
   @keys [
     :status,
     :body,
@@ -12,8 +14,8 @@ defmodule Snap.HTTPError do
   @enforce_keys @keys
   defexception @keys
 
-  def exception_from_response(%Finch.Response{status: status, body: body, headers: headers}) do
-    struct(__MODULE__, %{status: status, body: body, headers: headers})
+  def exception_from_response(%HTTPClient.Response{status: status, body: body, headers: headers}) do
+    %__MODULE__{status: status, body: body, headers: headers}
   end
 
   def message(%__MODULE__{} = e) do
--- lib/snap/supervisor.ex
+++ lib/snap/supervisor.ex
@@ -2,8 +2,8 @@ defmodule Snap.Cluster.Supervisor do
   @moduledoc false
 
   use Supervisor
-  @default_pool_size 5
-  @default_conn_opts []
+
+  alias Snap.HTTPClient
 
   def start_link(cluster, otp_app, config) do
     Supervisor.start_link(__MODULE__, {cluster, otp_app, config}, name: cluster)
@@ -13,37 +13,29 @@ defmodule Snap.Cluster.Supervisor do
     Snap.Config.get(config_name(cluster))
   end
 
-  def connection_pool_name(cluster) do
-    Module.concat(cluster, Pool)
-  end
-
   ## Callbacks
 
   @doc false
   @impl Supervisor
   def init({cluster, _otp_app, config}) do
-    children = [
-      {Finch, finch_config(cluster, config)},
-      {Snap.Config, {config_name(cluster), config}}
-    ]
+    children =
+      [
+        {Snap.Config, {config_name(cluster), config}}
+      ] ++ maybe_initialize_http_client(cluster, config)
 
     Supervisor.init(children, strategy: :one_for_one)
   end
 
-  defp finch_config(cluster, config) do
-    url = Keyword.fetch!(config, :url)
-    size = Keyword.get(config, :pool_size, @default_pool_size)
-    conn_opts = Keyword.get(config, :conn_opts, @default_conn_opts)
-
-    [
-      name: connection_pool_name(cluster),
-      pools: %{
-        url => [size: size, count: 1, conn_opts: conn_opts]
-      }
-    ]
-  end
-
   defp config_name(cluster) do
     Module.concat(cluster, Config)
+  end
+
+  defp maybe_initialize_http_client(cluster, config) do
+    config = Keyword.put(config, :cluster, cluster)
+
+    case HTTPClient.child_spec(config) do
+      :skip -> []
+      http_client_child_spec -> [http_client_child_spec]
+    end
   end
 end
