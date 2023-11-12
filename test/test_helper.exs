93c76eac685e051e3dad07b9c310af39a88bc58d
Tom Taylor <tom@tomtaylor.co.uk>
2023-11-12 21:10:04 +0000

    Use Elixir 1.12 compatible Logger level set

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,6 @@
 ExUnit.configure(exclude: [integration: true])
 
-Logger.put_application_level(:snap, :warning)
+Logger.configure(level: :warning)
 
 url = "http://localhost:9200"
 {:ok, _} = Snap.Test.Cluster.start_link(url: url, index_namespace: "snap-test")
